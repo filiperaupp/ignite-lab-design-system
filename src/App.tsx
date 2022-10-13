d4b8cf6881bd1e1f3abef9c6d836bdb4b1dea880
Filipe Raupp <filipe.raupp@hotmail.com>
2022-10-13 15:18:15 -0300

    innitial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./styles/global.css";
+
+export function App() {
+  return (
+    <>
+      <h1 className="font-bold text-2xl text-purple-500">Hello world</h1>
+      <button className="bg-cyan-500 px-4 py-2">Teste</button>
+    </>
+  );
+}
